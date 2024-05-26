c104b864b81dd623a2a9ab52b6f59d35760dddb5
AdkPrtm <andhikapratama685@gmail.com>
2024-05-26 16:04:17 +0800

    Add: cronjob on vercel

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,12 +1,9 @@
 import { checkDeadline } from "@/lib/cronjob";
 import { NextResponse } from "next/server";
-import cron from "node-cron";
 
 export async function POST() {
     try {
-        cron.schedule('30 1 * * *', async () => {
-            checkDeadline()
-        });
+        await checkDeadline()
         return NextResponse.json({ data: 'Success', status: 200 });
     } catch (error) {
         return NextResponse.json({ error: error }, { status: 500 })
