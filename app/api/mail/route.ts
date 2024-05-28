c554e8ae977704d7b44c96eb8e12f5142cbb3ab2
AdkPrtm <andhikapratama685@gmail.com>
2024-05-28 10:24:28 +0800

    fix: nodemailer in cronjob

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,11 +1,14 @@
-import { checkDeadline } from "@/lib/cronjob";
-import { NextResponse } from "next/server";
+import { checkDeadline } from '@/lib/cronjob';
+import type { NextRequest } from 'next/server';
 
-export async function GET() {
-    try {
-        await checkDeadline()
-        return NextResponse.json({ data: 'Success', status: 200 });
-    } catch (error) {
-        return NextResponse.json({ error: error }, { status: 500 })
+export async function GET(request: NextRequest) {
+    const authHeader = request.headers.get('authorization');
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return new Response('Unauthorized', {
+            status: 401,
+        });
     }
+    await checkDeadline()
+
+    return Response.json({ success: true });
 }
--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -2,44 +2,59 @@ import { OptionsMailOTP, OptionsReminderMail } from "@/constants/constants";
 import { SendMailOTPParams, SendMailReminderParams } from "@/types/types";
 import nodemailer from "nodemailer";
 
+const ConfNodemailer = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+    },
+})
 export async function SendMailOTP(params: SendMailOTPParams) {
-    const transport = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASSWORD,
-        },
-    })
+    await new Promise((resolve, reject) => {
+        ConfNodemailer.verify(function (error, success) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(success);
+            }
+        });
+    });
 
-    try {
-        const result = await transport.sendMail(OptionsMailOTP(params))
-        if (result.messageId) {
-            return { success: 'Success, sent email' }
-        }
-    } catch (error) {
-        return { error: 'Failed to send email' }
-    }
+    await new Promise((resolve, reject) => {
+        ConfNodemailer.sendMail(OptionsMailOTP(params), (err, info) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(info);
+            }
+        });
+    });
 }
 
 export async function SendMailReminder(params: SendMailReminderParams) {
-    const transport = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASSWORD,
-        },
-    })
+    await new Promise((resolve, reject) => {
+        ConfNodemailer.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
 
-    try {
-        const result = await transport.sendMail(OptionsReminderMail(params))
-        if (result.messageId) {
-            return { success: 'Success, sent email' }
-        }
-    } catch (error) {
-        return { error: 'Failed to send email' }
-    }
+    await new Promise((resolve, reject) => {
+        ConfNodemailer.sendMail(OptionsReminderMail(params), (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                console.log(info);
+                resolve(info);
+            }
+        });
+    });
 }
